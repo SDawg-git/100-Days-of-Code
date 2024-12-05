0b9c1f8a382ea5b9d048ca8633530eefb4d677fa
SDawg-git <woloncewiczszymon@gmail.com>
2024-12-05 17:55:42 +0000

    Create bandnamegen.py
    day 1 of 100 days of code

---
+++ day-1-Band_Name_Generator/bandnamegen.py
@@ -0,0 +1,4 @@
+print("Hello, please answer the following questions to generate your band name")
+cityname = input("What city were you born in?\n")
+petname = input("What's your pet's name?\n")
+print("Your band name is", cityname + " " + petname)
