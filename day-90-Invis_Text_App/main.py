ebd21d119108b99f393ef497ea116631a133f4d2
SDawg-git <woloncewiczszymon@gmail.com>
2025-03-18 19:47:37 +0000

    Update main.py

--- day-90-Invis_Text_App/main.py
+++ day-90-Invis_Text_App/main.py
@@ -1,1 +1,22 @@
-#upload later
+from flask import Flask, render_template, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+@app.route("/")
+def home():
+
+
+    return render_template("index.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
