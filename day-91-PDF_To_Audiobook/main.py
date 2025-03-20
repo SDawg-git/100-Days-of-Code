edeb50968165e4c2ab44d5eb2f3e6008a52990ca
SDawg-git <woloncewiczszymon@gmail.com>
2025-03-20 03:26:27 +0000

    Update main.py

--- day-91-PDF_To_Audiobook/main.py
+++ day-91-PDF_To_Audiobook/main.py
@@ -1,1 +1,16 @@
-#upload code when back
+from PyPDF2 import PdfReader
+from gtts import gTTS
+
+user = "YOUR DESKTOP USERNAME"
+
+pdf_file = f'C:\\Users\\{user}\\Desktop\\tts_test.pdf'    #link to pdf
+
+
+reader = PdfReader(pdf_file)
+page = reader.pages[0]
+extracted_text = page.extract_text()
+
+
+tts = gTTS(extracted_text)
+tts.save('C:\\Users\\dzemo\\Desktop\\tts_test.mp3')
+
