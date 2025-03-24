b7f6e541ae40eeb0b16e516e13c89d95b6882159
SDawg-git <woloncewiczszymon@gmail.com>
2025-03-24 20:51:37 +0000

    Update main.py

--- day-93-Custom_Web_Scraper/main.py
+++ day-93-Custom_Web_Scraper/main.py
@@ -1,10 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
-website_url = "https://news.ycombinator.com/news"
+service = Service(ChromeDriverManager().install())
+
+website_url = "https://steamdb.info"
+driver = webdriver.Chrome(service=service)      # had to use a driver as the webpage JavaScript was not rendering
+driver.get(website_url)
+
+
+
 response = requests.get(url=website_url)
 webpage = response.text
 
-soup = BeautifulSoup(webpage, "html.parser")
+soup = BeautifulSoup(driver.page_source, "html.parser")
 
-print(soup)
+games_info = soup.find_all(class_="css-truncate")
+print(games_info)
+
+#since css-truncate contained all the titles on the page, I've decided to manually cut off the loop after 15 games
+#as that's the maximum of games that are shown at once
+
+count = 15
+with open ("most_played_games.csv", "w") as file:
+
+    for game_title in games_info:
+        if count > 0:
+            try:
+                file.write(game_title.get_text(strip=True))
+                file.write("\n")
+                count-=1
+            except(UnicodeError):   #russian titles caused unicode error so they will be skipped
+                count-=1
