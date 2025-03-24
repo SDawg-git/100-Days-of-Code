2210b450e3f3c9d57c0f84f3d4b43e06128642f3
SDawg-git <woloncewiczszymon@gmail.com>
2025-03-24 19:59:40 +0000

    Update main.py

--- day-93-Custom_Web_Scraper/main.py
+++ day-93-Custom_Web_Scraper/main.py
@@ -1,1 +1,10 @@
-#upload when back 
+from bs4 import BeautifulSoup
+import requests
+
+website_url = "https://news.ycombinator.com/news"
+response = requests.get(url=website_url)
+webpage = response.text
+
+soup = BeautifulSoup(webpage, "html.parser")
+
+print(soup)
