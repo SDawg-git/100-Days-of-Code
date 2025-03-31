3538440316465c3e93c15ca9eae6ad93d6591b46
SDawg-git <woloncewiczszymon@gmail.com>
2025-03-31 20:29:36 +0100

    Update projectile.py

--- day-95-Space_Invaders/projectile.py
+++ day-95-Space_Invaders/projectile.py
@@ -1,13 +1,20 @@
 from turtle import Turtle
 
 class Projectile(Turtle):
-    def __init__(self):
+    def __init__(self, start_x, start_y):
         super().__init__()
 
         self.penup()
         self.shape("square")
         self.color("white")
         self.shapesize(0.75, 0.25)
+        self.setposition(x=0, y=-220)
+        self.goto(start_x, start_y)
+        self.speed = 5
+        self.active = True
 
-
-
+    def move_projectile(self):
+        if self.ycor() < 270:
+            self.sety(self.ycor() + self.speed)
+        else:
+            self.active = False
