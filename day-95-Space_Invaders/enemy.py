f5e27a554e3ebdc27b46b65f8a719976132d023f
SDawg-git <woloncewiczszymon@gmail.com>
2025-04-01 02:27:14 +0100

    Update enemy.py

--- day-95-Space_Invaders/enemy.py
+++ day-95-Space_Invaders/enemy.py
@@ -1,14 +1,27 @@
 from turtle import Turtle
-from projectile import Projectile
 
 class Enemy(Turtle):
-    def __init__(self):
+    def __init__(self, start_x, start_y):
         super().__init__()
-
-
         self.penup()
         self.shape("turtle")
         self.color("white")
         self.tiltangle(270)
-        self.setposition(x=0, y=0)
+        self.goto(start_x, start_y)
+        self.direction = 1
+        self.speed = 4
+        self.left_wall = -180
+        self.right_wall = 180
+        self.move_down_distance = 20
 
+    def move(self):
+        new_x = self.xcor() + (self.direction * self.speed)  #calculating new X is better than using self.forward due to movement direction complicating the game
+
+        if new_x > self.right_wall or new_x < self.left_wall: #changes on wall detection
+            self.change_direction()
+        else:
+            self.goto(new_x, self.ycor())
+
+    def change_direction(self):
+        self.direction *= -1  #flip direction
+        self.goto(self.xcor(), self.ycor() - self.move_down_distance)  #moves down
