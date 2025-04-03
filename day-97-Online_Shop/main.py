9eb0d8dd65e6021caf2f7d898e84408034a6277d
SDawg-git <woloncewiczszymon@gmail.com>
2025-04-03 02:07:08 +0100

    Update main.py

--- day-97-Online_Shop/main.py
+++ day-97-Online_Shop/main.py
@@ -1,1 +1,54 @@
-#upload when back
+from datetime import date
+from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column, foreign
+from sqlalchemy import Integer, String, Text
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap5(app)
+
+
+
+# TODO: Configure Flask-Login
+#flasks login manager config
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+#user callback
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CONFIGURE TABLES
+class User(db.Model):
+    __tablename__ = "Users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+class Product(db.Model):
+    __tablename__ = "Products"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    item_name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    item_price: Mapped[int] = mapped_column(Integer, nullable=False)
+    item_picture: Mapped[str] = mapped_column(String(250), nullable=False)
