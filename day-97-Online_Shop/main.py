22c76ed394e937cb0c7bf279d5503663273154b6
SDawg-git <woloncewiczszymon@gmail.com>
2025-04-06 17:15:15 +0100

    Update main.py

--- day-97-Online_Shop/main.py
+++ day-97-Online_Shop/main.py
@@ -2,12 +2,14 @@ from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
-from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, user_logged_in
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column, foreign
 from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
+from forms import RegisterForm, LoginForm
+
 
 
 
@@ -43,14 +45,14 @@ def admin_only(func):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 
 # CONFIGURE USER TABLE
-class User(db.Model):
-    __tablename__ = "Users"
+class User(db.Model, UserMixin):
+    __tablename__ = "User"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
@@ -58,18 +60,18 @@ class User(db.Model):
 
 # CONFIGURE PRODUCT TABLE
 class Product(db.Model):
-    __tablename__ = "Products"
+    __tablename__ = "Product"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     item_name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     item_description = db.Column(db.Text, nullable=True)
-    item_price: Mapped[int] = mapped_column(Integer, nullable=False)
+    item_price: Mapped[float] = mapped_column(Integer, nullable=False)
     item_picture: Mapped[str] = mapped_column(String(250), nullable=False)
 
 # CONFIGURE CART TABLE
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey("Product.id"), nullable=False)
     quantity = db.Column(db.Integer, default=1)
 
 
@@ -77,8 +79,30 @@ with app.app_context():
     db.create_all()
 
 
+# new_product = Product(
+#     item_name = "Sword",
+#     item_description = "Long and sharp",
+#     item_price = 299.99,
+#     item_picture = "https://cdn11.bigcommerce.com/s-97i9gwv/images/stencil/960w/products/8964/129604/3fmyyejhh4m__67956.1706520889.jpg?c=2"
+# )
+#
+# with app.app_context():
+#     db.session.add(new_product)
+#     db.session.commit()
 
-# TODO: Use Werkzeug to hash the user's password when creating a new user.
+@app.route('/')
+def home():
+
+    result = db.session.execute(db.select(Product))
+    all_products = result.scalars().all()
+
+
+
+    return render_template("index.html", user = current_user, products = all_products)
+
+
+
+
 @app.route('/register', methods=["GET", "POST"])
 def register():
     form = RegisterForm()
@@ -105,13 +129,87 @@ def register():
             new_user = User(
                 email = usermail,
                 password = hashed_pass,
-                name = username,
+                username = username,
             )
 
             db.session.add(new_user)
             db.session.commit()
 
             login_user(new_user)
-            return redirect(url_for("get_all_posts"))
+            return redirect(url_for("home"))
 
     return render_template("register.html", form = form, user=current_user)
+
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+
+    form = LoginForm()
+
+    if request.method == "POST":
+
+        entered_email = request.form["email"]
+        entered_pass = request.form["password"]
+
+        result = db.session.execute(db.select(User).where(User.email == entered_email))
+        user = result.scalar()
+
+        if not user:
+            flash("Email does not exist.")
+            return redirect(url_for("login"))
+        else:
+
+            if check_password_hash(pwhash=user.password, password=entered_pass):
+
+                login_user(user)
+                flash("Logged in Successfully")
+
+                return redirect(url_for("home", user = current_user))
+
+            else:
+                flash("Incorrect Password")
+                return redirect(url_for("login"))
+
+
+    return render_template("login.html", form=form, user=current_user)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('get_all_posts'))
+
+
+
+@app.route('/add-to-cart', methods=["POST"])
+def add_to_cart():
+
+
+    if not current_user.is_authenticated:
+        print("not logged in")
+        return redirect(url_for('login'))
+    else:
+        product_id = request.form['product_id']
+        quantity = request.form['quantity']
+        user_id = current_user.id
+        print(product_id)
+        print(quantity)
+        print(user_id)
+
+        result = db.get_or_404(Product, product_id)
+        print(result.item_name)
+
+        #checks if cart exists, if not creates it
+        cart_check = db.session.get(Cart, user_id)
+        if not cart_check:
+            print("cart no exist")
+        else:
+            print("exists")
+
+        return redirect(url_for('home'))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
